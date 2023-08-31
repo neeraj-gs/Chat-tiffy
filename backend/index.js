0d6e4061fa43dd5d0b3b957a77b3e212900095e5
neeraj-gs <gsneeraj05@gmail.com>
2023-08-31 11:58:21 +0530

    Added the first route to check our backejnd

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,14 @@ const port = 3000;
 const app = express();
 app.use(cors());
 
+app.post("/authenticate",async(req,res)=>{
+    const {username} = req.body;
+    return res.json({
+        username:username,
+        secret:"sha256..."
+    })
+})
+
 
 app.listen(port,()=>{
     console.log(`Server Running on Port:${port}`);
